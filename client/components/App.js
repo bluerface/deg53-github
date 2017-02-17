be6d4a6aeeb1bc877c785919d6a7e592f2bfa902
bluerface <fallingsilkworm@gmail.com>
2017-02-17 17:08:28 +0000

    Initial commit with boilerplate react app

---
+++ client/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Main.scss';
+
+const App = (props) => {
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+};
+
+App.propTypes = {};
+
+export default App;
