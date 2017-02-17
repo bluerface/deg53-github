5fd560bc6818dc7d99d300983ee74f8b752d5316
bluerface <fallingsilkworm@gmail.com>
2017-02-17 19:34:14 +0000

    Make the search button dispatch a search request

--- client/components/SearchBar.js
+++ client/components/SearchBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import actions from '../redux/actions';
 
 const SearchBar = (props) => {
   return (
     <div className='input-group'>
       <input type='text' className='form-control' placeholder='Search for Repo...' />
       <span className='input-group-btn'>
-        <button className='btn btn-default' type='button'>
+        <button className='btn btn-default' type='button' onClick={props.search}>
           <i className='fa fa-search' />
         </button>
       </span>
@@ -14,7 +16,13 @@ const SearchBar = (props) => {
 };
 
 SearchBar.propTypes = {
-
+  search: React.PropTypes.func.isRequired
 };
 
-export default SearchBar;
+const mapDispatchToProps = (dispatch) => ({
+  search () {
+    dispatch(actions.fetchSearchResults());
+  }
+});
+
+export default connect(null, mapDispatchToProps)(SearchBar);
--- client/components/SearchResults.js
+++ client/components/SearchResults.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import actions from '../redux/actions/repos.actions.js';
 
 var SearchResults = (props) => {
   console.log(props.repos);
@@ -12,20 +11,13 @@ var SearchResults = (props) => {
 };
 
 SearchResults.propTypes = {
-  repos: React.PropTypes.object.isRequired,
-  test: React.PropTypes.func.isRequired
+  repos: React.PropTypes.object.isRequired
 };
 
 const mapStateToProps = (state) => ({
   repos: state.repos
 });
 
-const mapDispatchToProps = dispatch => ({
-  test () {
-    dispatch(actions.placeholder());
-  }
-});
-
-SearchResults = connect(mapStateToProps, mapDispatchToProps)(SearchResults);
+SearchResults = connect(mapStateToProps)(SearchResults);
 
 export default SearchResults;
