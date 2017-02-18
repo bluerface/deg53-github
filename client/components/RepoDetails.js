a7b9a1c4d5bfcf0a5a2708c11d2a4aa3dd871c7a
bluerface <fallingsilkworm@gmail.com>
2017-02-18 17:54:48 +0000

    Fetch and Render readme on details page

--- client/components/RepoDetails.js
+++ client/components/RepoDetails.js
@@ -1,15 +1,47 @@
 import React from 'react';
-// import '../styles/RepoDetails.scss';
+import {connect} from 'react-redux';
+import actions from '../redux/actions';
+import '../styles/RepoDetails.scss';
 
-var RepoDetails = (props) => {
-  return (
-    <div className='repo-card'>
-      Details for {props.params.id}
-    </div>
-  );
-};
+class RepoDetails extends React.Component {
+  componentWillMount () {
+    var id = this.props.params.id;
+    var fullname = this.props.repo.full_name;
+    this.props.getReadme(fullname, id);
+  }
+  render () {
+    var {repo, readme, ui} = this.props;
+    return (
+      <div className=''>
+        <h3>{repo.full_name}</h3>
+        {ui.loading && 'Loading...'}
+        {ui.results &&
+          <div className='readme'>
+            <h3>
+              <i className='fa fa-book' style={{paddingRight: '0.5em'}} />
+              Readme.md
+            </h3>
+            <div dangerouslySetInnerHTML={{__html: readme}} />
+          </div>
+        }
+      </div>
+    );
+  }
+}
 
 RepoDetails.propTypes = {
 };
 
-export default RepoDetails;
+const mapStateToProps = (state, ownProps) => ({
+  repo: state.entities.repos.byId[ownProps.params.id],
+  readme: state.entities.readmes.byId[ownProps.params.id],
+  ui: state.ui.readme
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getReadme (fullname, id) {
+    dispatch(actions.fetchReadme(fullname, id));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(RepoDetails);
