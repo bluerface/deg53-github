9dda3c3141884e21a7b5ad8aad72262aedb43eee
bluerface <fallingsilkworm@gmail.com>
2017-02-19 14:39:14 +0000

    Fix issue with page reload and persist state

--- client/initStore.js
+++ client/initStore.js
@@ -1,14 +1,28 @@
 import {createStore, applyMiddleware} from 'redux';
 import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
+import {debounce} from 'underscore';
 
 import reducer from './redux/reducers';
 
 function initStore () {
+  var state = window.localStorage.getItem('state');
+  state = state ? JSON.parse(state) : undefined;
+
   let store = createStore(
     reducer,
+    state,
     applyMiddleware(thunk, createLogger())
   );
+
+  function cacheStore () {
+    window.localStorage.setItem(
+      'state',
+      JSON.stringify(store.getState())
+    );
+  }
+
+  store.subscribe(debounce(cacheStore, 500));
 
   return store;
 }
